bd8b076cebb2a8ea28a4e3a2af68378b1f8f0515
Geena <geena_smith@hotmail.com>
2018-11-23 10:33:51 -0800

    add mapping from projects to user

--- scripts/dbpopulate.py
+++ scripts/dbpopulate.py
@@ -12,41 +12,52 @@ usecases = db.usecases
 # -------------------- users --------------------
 
 # users json
-admin = {"name":"Admin", "role":"administrator", "projects":[]}
-tristan = {"name":"Tristan", "role":"user", "projects":[]}
-matt = {"name":"Matt", "role":"user", "projects":[]}
-amrit = {"name":"Amrit", "role":"user", "projects":[]}
-geena = {"name":"Geena", "role":"user", "projects":[]}
-jens = {"name":"Dr Jens Weber - UML Master", "role":"user", "projects":[]}
-
-user_ids = users.insert_many([admin,tristan, matt, amrit, geena, jens])
-
+admin = users.insert_one({"name":"Admin", "role":"administrator", "projects":[]})
+tristan = users.insert_one({"name":"Tristan", "role":"user", "projects":[]})
+matt = users.insert_one({"name":"Matt", "role":"user", "projects":[]})
+amrit = users.insert_one({"name":"Amrit", "role":"user", "projects":[]})
+geena = users.insert_one({"name":"Geena", "role":"user", "projects":[]})
+jens = users.insert_one({"name":"Dr Jens Weber - UML Master", "role":"user", "projects":[]})
+ 
 
 # -------------------- projects --------------------
 
 # project json
-p1 = {"title":"project 1", "useCases":[]}
-p2 = {"title":"project 2", "useCases":[]}
-p3 = {"title":"project 3", "useCases":[]}
+p1 = {"title":"project 1", "users":[], "useCases":[]}
+p2 = {"title":"project 2", "users":[], "useCases":[]}
+p3 = {"title":"project 3", "users":[], "useCases":[]}
 
 p1i = projects.insert_one(p1)
 p2i = projects.insert_one(p2)
 p3i = projects.insert_one(p3)
 
 #tristan=owner, Matt=read, Amrit=write
-users.update_one({"name":"Tristan"},{ '$push': {"projects": {'_id':p1i.inserted_id,"permission":"owner"}} })
-users.update_one({"name":"Matt"},{ '$push': {"projects": {'_id':p1i.inserted_id,"permission":"read"}} })
-users.update_one({"name":"Amrit"},{ '$push': {"projects": {'_id':p1i.inserted_id,"permission":"write"}} })
+users.update_one({"name":"Tristan"},{ '$push': {"projects": {"_id":p1i.inserted_id,"permission":"owner"}} })
+users.update_one({"name":"Matt"},{ '$push': {"projects": {"_id":p1i.inserted_id,"permission":"read"}} })
+users.update_one({"name":"Amrit"},{ '$push': {"projects": {"_id":p1i.inserted_id,"permission":"write"}} })
 
 #matt=owner, tristan=read, amrit=read
-users.update_one({"name":"Matt"},{ '$push': {"projects": {'_id':p2i.inserted_id,"permission":"owner"}} })
-users.update_one({"name":"Tristan"},{ '$push': {"projects": {'_id':p2i.inserted_id,"permission":"read"}} })
-users.update_one({"name":"Amrit"},{ '$push': {"projects": {'_id':p2i.inserted_id,"permission":"read"}} })
+users.update_one({"name":"Matt"},{ '$push': {"projects": {"_id":p2i.inserted_id,"permission":"owner"}} })
+users.update_one({"name":"Tristan"},{ '$push': {"projects": {"_id":p2i.inserted_id,"permission":"read"}} })
+users.update_one({"name":"Amrit"},{ '$push': {"projects": {"_id":p2i.inserted_id,"permission":"read"}} })
 
 #geena=owner, tristan=write, amrit=write
-users.update_one({"name":"Geena"},{ '$push': {"projects": {'_id':p3i.inserted_id,"permission":"owner"}} })
-users.update_one({"name":"Tristan"},{ '$push': {"projects": {'_id':p3i.inserted_id,"permission":"write"}} })
-users.update_one({"name":"Amrit"},{ '$push': {"projects": {'_id':p3i.inserted_id,"permission":"read"}} })
+users.update_one({"name":"Geena"},{ '$push': {"projects": {"_id":p3i.inserted_id,"permission":"owner"}} })
+users.update_one({"name":"Tristan"},{ '$push': {"projects": {"_id":p3i.inserted_id,"permission":"write"}} })
+users.update_one({"name":"Amrit"},{ '$push': {"projects": {"_id":p3i.inserted_id,"permission":"read"}} })
+
+# map projects to users
+projects.update_one({"title":"project 1"},{'$push': {"users": {"_id":tristan.inserted_id, "permission":"owner"}}})
+projects.update_one({"title":"project 1"},{'$push': {"users": {"_id":matt.inserted_id, "permission":"read"}}})
+projects.update_one({"title":"project 1"},{'$push': {"users": {"_id":amrit.inserted_id, "permission":"write"}}})
+
+projects.update_one({"title":"project 2"},{'$push': {"users": {"_id":matt.inserted_id, "permission":"owner"}}})
+projects.update_one({"title":"project 2"},{'$push': {"users": {"_id":tristan.inserted_id, "permission":"read"}}})
+projects.update_one({"title":"project 2"},{'$push': {"users": {"_id":amrit.inserted_id, "permission":"read"}}})
+
+projects.update_one({"title":"project 3"},{'$push': {"users": {"_id":geena.inserted_id, "permission":"owner"}}})
+projects.update_one({"title":"project 3"},{'$push': {"users": {"_id":tristan.inserted_id, "permission":"write"}}})
+projects.update_one({"title":"project 3"},{'$push': {"users": {"_id":amrit.inserted_id, "permission":"write"}}})
 
 
 # -------------------- use cases --------------------
@@ -82,7 +93,7 @@ u2ai = usecases.insert_one(u2a)
 u3ai = usecases.insert_one(u3a)
 
 # project 1 has 2 use cases, projects 2 and 3 each have 1
-projects.update_one({"title":"project 1"},{ '$push': {"useCases": {'_id':u1ai.inserted_id}} })
-projects.update_one({"title":"project 1"},{ '$push': {"useCases": {'_id':u1bi.inserted_id}} })
-projects.update_one({"title":"project 2"},{ '$push': {"useCases": {'_id':u2ai.inserted_id}} })
-projects.update_one({"title":"project 3"},{ '$push': {"useCases": {'_id':u3ai.inserted_id}} })
+projects.update_one({"title":"project 1"},{ '$push': {"useCases": {"_id":u1ai.inserted_id}} })
+projects.update_one({"title":"project 1"},{ '$push': {"useCases": {"_id":u1bi.inserted_id}} })
+projects.update_one({"title":"project 2"},{ '$push': {"useCases": {"_id":u2ai.inserted_id}} })
+projects.update_one({"title":"project 3"},{ '$push': {"useCases": {"_id":u3ai.inserted_id}} })
